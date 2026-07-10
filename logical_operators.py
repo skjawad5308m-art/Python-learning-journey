9819a9abac2f7f6c0ce93a543a768a48e53361e0
Md.Sujnain Jawad <skjawad5308m@gmail.com>
2026-07-10 16:03:12 +0600

    Add files via upload

---
+++ logical_operators.py
@@ -0,0 +1,15 @@
+a = 56
+
+b = 48
+
+print(a > b)
+print(a < b)
+
+print(not (a > b))
+print(not (a < b))
+
+vl1 = True
+vl2 = False
+
+print("AND operator:", vl1 & vl2)
+print("OR operator:", vl1 & vl2)
