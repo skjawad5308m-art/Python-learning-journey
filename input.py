52fe8668c57b107c841d72266415f81fc8d67234
Md.Sujnain Jawad <skjawad5308m@gmail.com>
2026-07-11 19:10:53 +0600

    Add files via upload

---
+++ input.py
@@ -0,0 +1,13 @@
+name = input("Enter your name:")
+
+age = input("Enter your age:")
+
+nationality = input("Enter your nationality:")
+
+print("Welcome", name)
+
+print("Your age is", age)
+
+print("you are", nationality)
+
+
