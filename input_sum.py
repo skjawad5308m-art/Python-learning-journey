43dee960c69d34627a397bc91a7c79ae86efaf26
Md.Sujnain Jawad <skjawad5308m@gmail.com>
2026-07-12 11:35:20 +0600

    Add files via upload

---
+++ input_sum.py
@@ -0,0 +1,7 @@
+first = int(input("Enter the first number:"))
+second = int(input("Enter the second number:"))
+
+print("sum =", first + second)
+print("diff =", first - second)
+print("product =", first * second)
+print("quotient =", first / second)
