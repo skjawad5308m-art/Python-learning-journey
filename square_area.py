a9f2cc3975a25990c0f8fce9f63cf894bd417199
Md.Sujnain Jawad <skjawad5308m@gmail.com>
2026-07-12 11:50:13 +0600

    Add files via upload

---
+++ square_area.py
@@ -0,0 +1,10 @@
+side = int(input("Enter the side of square:"))
+print("product",side*side)
+
+length  = int(input("Enter the length of rectangle:"))
+width = int(input("Enter the width of rectangle:"))
+print("product",length*width)
+
+base = int(input("Enter the base of triangle:"))
+height = int(input("Enter the height of triangle:"))
+print("product", 0.5 * base * height)
