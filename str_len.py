da57c377e119fd1aa6f9c21bc2469a5b5ecdfc04
Md.Sujnain Jawad <skjawad5308m@gmail.com>
2026-07-14 17:58:58 +0600

    Add files via upload

---
+++ str_len.py
@@ -0,0 +1,18 @@
+str1 = "My name is Jawad,\n I am a student."
+print(str1)
+print(len(str1))
+
+str2 = "I am studying in DHAKA RESIDENTIAL MODEL COLLEGE,\t I live in Dhaka,Bangladesh"
+print(str2)
+print(len(str2))
+
+str3 = "The world"
+str4 = "is very beautiful."
+final_str = str3 +" " + str4
+print(final_str)
+print(len(final_str))
+
+str5 = ("I have a dream for which I am working hard and also dealing with a lot of problems."
+        "I hope one day my dream will come true as it says HARD WORK PAYS OFF.")
+print(str5)
+print(len(str5))
