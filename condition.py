3eec79b3ec6a423e186bea013a8b56f226c5162f
Md.Sujnain Jawad <skjawad5308m@gmail.com>
2026-07-17 21:23:28 +0600

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,18 @@
+marks = int(input("Enter your marks:"))
+
+if(marks >= 90):
+    print("Your grade is A")
+    print("Excellent")
+elif(marks >= 80):
+    print("Your grade is B")
+    print("Very Good")
+elif(marks >= 70):
+    print("Your grade is C")
+    print("Good")
+elif(marks >= 60):
+    print("Your grade is D")
+    print("Needs Improvement")
+else:
+    print("Your grade is F")
+    print("LACK OF DISCIPLINE AND HARD WORK")
+    print("Very Poor performance")
