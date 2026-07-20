f4f056ccb22cdfa3a5f1d8421bbfbd90e977a833
Md.Sujnain Jawad <skjawad5308m@gmail.com>
2026-07-20 21:26:33 +0600

    Add files via upload

---
+++ adult_teen identification.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age:"))
+if(age >= 18):
+    print("You are adult")
+    print("You can vote")
+    print("You can have driving liscence")
+
+else:
+    print("You are a kid")
+    print("You cannot vote")
+    print("You cannot have driving liscence")
+    print("you cannot travell")
