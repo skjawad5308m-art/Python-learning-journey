c4b2b9c7f2dedba596f8d38ef87210e4e35828aa
Md.Sujnain Jawad <skjawad5308m@gmail.com>
2026-07-21 17:43:00 +0600

    Add files via upload

---
+++ guess the number.py
@@ -0,0 +1,10 @@
+num1 = 100
+num2 = 96
+num3 =80
+
+guess1 = input("Enter 1st number :")
+guess2 = input("Enter 2nd number :")
+guess3 = input("Enter 3rd number :")
+
+if("User types guess1 == num1 and guess2 == num2 and guess3 == num3"):
+    print("You have gussed all the numbers correctly")
