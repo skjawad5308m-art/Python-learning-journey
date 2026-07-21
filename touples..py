9baa4c59ff2ea109b8d295e1fbeeede01679d94c
Md.Sujnain Jawad <skjawad5308m@gmail.com>
2026-07-21 16:37:57 +0600

    Add files via upload

---
+++ touples..py
@@ -0,0 +1,11 @@
+places = []
+
+place1 = input("Enter your 1st favourite place:")
+place2 = input("Enter your 2nd favourite place:")
+place3 = input("Enter you third favourite place;")
+
+places.append(place1)
+places.append(place2)
+places.append(place3)
+
+print(places)
