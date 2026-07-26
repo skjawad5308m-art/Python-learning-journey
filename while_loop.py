9383a163b21387f8ee9a7da94c7364654fcf91d0
Md.Sujnain Jawad <skjawad5308m@gmail.com>
2026-07-26 16:55:07 +0600

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,31 @@
+i1 = 1000
+while i1 >= 1:
+    print("PROTECT THOSE YOU LOVE", i1)
+    i1 = i1 -1
+    print("THE LOOP HAS ENDED")
+print(i1)
+print(type(i1))
+
+i2 = 1
+while i2 <= 1000:
+    print("SOMETIMES YOU NEED TO CHASE YOUR DREAMS TO BE HAPPY", i2)
+    i2 = i2 + 1
+    print("THE LOOP HAS ENDED")
+print(i2)
+print(type(i2))
+
+i3 = 1
+while i3 >= -1000:
+    print("EVERYTHING FOR FAMILY", i3)
+    i3 = i3 - 1
+    print("THE LOOP HAS ENDED")
+    print(i3)
+    print(type(i3))
+
+i4 = -1000
+while i4 <= 1000:
+    print("DUTIES ON MY SHOULDERS", i4)
+    i4 = i4 - 1
+    print("THE LOOP HAS ENDED")
+    print(i4)
+print(type(i4))
