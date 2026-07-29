ec3b66ddbce0fa7dc7a39152e95911e690ae7d68
Md.Sujnain Jawad <skjawad5308m@gmail.com>
2026-07-29 20:09:15 +0600

    Add files via upload

---
+++ while.loop 3.py
@@ -0,0 +1,25 @@
+n = int(input("ENTER THE NUMBER :"))
+i = 1
+while i <= 1000:
+    print(i * n )
+    i += 1
+
+n2 = int(input("ENTER THE 2ND NUMBER :"))
+i2 = 1000
+while i2 >= 1 :
+    print(i2 / n2)
+    i2 -= 1
+
+n3 = int(input("ENTER THE 3RD NUMBER :"))
+i3 = -1000
+while i3 <= 1000 :
+    print(i3 + n3)
+    i3 += 1
+
+n4 = int(input("ENTER THE 4TH NUMBER :"))
+i4 = 1000
+while i4 >= -1000 :
+    print(i4 - n4)
+    i4 -= 1
+
+
