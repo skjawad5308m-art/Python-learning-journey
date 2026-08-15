e318a1e7326c09f7ffb68fc80c2023d279ac3d44
Md.Sujnain Jawad <skjawad5308m@gmail.com>
2026-08-15 10:49:26 +0600

    Add files via upload

---
+++ fuctions.py
@@ -0,0 +1,29 @@
+def calc_sum(a, b):
+    sum = a + b
+    print(sum)
+    return(sum)
+calc_sum(8, 4)
+
+def calc_diff(a , b):
+    diff = a - b
+    print(diff)
+    return(diff)
+calc_diff(8, 4)
+
+def calc_mul(a , b):
+    mul = a * b
+    print(mul)
+    return(mul)
+calc_mul(8 , 4)
+
+def calc_div(a , b):
+    div = a / b
+    print(div)
+    return(div)
+calc_div(8, 4)
+
+def calc_avg(a , b, c):
+    avg = (a + b + c) / 3
+    print(avg)
+    return(avg)
+calc_avg(8, 4, 3)
