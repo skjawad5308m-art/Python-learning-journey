a256ff8739ac1df692c9e56f78ae9ba889bcb20a
Md.Sujnain Jawad <skjawad5308m@gmail.com>
2026-08-25 15:12:12 +0600

    Add files via upload

---
+++ default parameters3.py
@@ -0,0 +1,24 @@
+countries = ["USA", "Switzerland", "Finland", "Norway", "Germany", "UK", "Netherlands", "Denmark"]
+
+names = ["Jawad", "Ayat", "Ibrahim", "Fatima"]
+
+print(countries)
+
+print(len(countries))
+
+print(countries[0])
+
+print(countries[-1])
+
+print(countries[-2], end=" ")
+
+print(countries[6], end=" ")
+
+print(names)
+print(len(names))
+print(names[1:])
+print(names[:])
+print(names[1:-1])
+print(names[0:3])
+print(names[0], end=" ")
+print(names[3], end=" ")
