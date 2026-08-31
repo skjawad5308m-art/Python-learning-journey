137abf55d502e122ae6e4d8379750cad63ab59d7
Md.Sujnain Jawad <skjawad5308m@gmail.com>
2026-08-31 14:38:04 +0600

    Add files via upload

---
+++ write.py
@@ -0,0 +1,4 @@
+f = open("aboutme.txt", "w")
+f.write("I like to travel naturally beautiful places, listening horror stories, songs and reading story books ")
+f.write("\nI want to study abroad")
+f.close()
