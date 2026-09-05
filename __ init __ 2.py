b8a682a815731ee0dc8e596fce81d28763434ac9
Md.Sujnain Jawad <skjawad5308m@gmail.com>
2026-09-05 18:12:48 +0600

    Add files via upload

---
+++ __ init __ 2.py
@@ -0,0 +1,10 @@
+class Cricketers:
+    def __init__(self, name, centuries,):
+        self.name = name
+        self.centuries = centuries
+c1 = Cricketers("Sachin Tendulkar", "100 centuries")
+c2 = Cricketers("Riki Ponting", "71 centuries")
+c3 = Cricketers("Kane Williamson", "48 centuries")
+print(c1.name, c1.centuries)
+print(c2.name,  c2.centuries)
+print(c3.name,  c3.centuries)
