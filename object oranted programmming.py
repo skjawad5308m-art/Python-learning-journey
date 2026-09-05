59596a0b4aa8843f269b088983e5e2ffa1b21985
Md.Sujnain Jawad <skjawad5308m@gmail.com>
2026-09-05 10:02:54 +0600

    Add files via upload

---
+++ object oranted programmming.py
@@ -0,0 +1,26 @@
+class Student:
+    name = "Jawad"
+    grade = "11 th grade"
+    year = "2026"
+    institution = "Dhaka Residential Model College"
+s1 = Student()
+print(s1.name)
+print(s1.grade)
+print(s1.year)
+print(s1.institution)
+
+class Cars:
+    color = "Black"
+    model = "BMW"
+c1 = Cars()
+print(c1.color)
+print(c1.model)
+
+class Cricketer:
+    player = "Kane Williamson"
+    country = "New Zealand"
+    role = "Bats man"
+p1 = Cricketer()
+print(p1.player)
+print(p1.country)
+print(p1.role)
